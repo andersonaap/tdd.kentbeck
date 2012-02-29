83564b351fd0152d9e7a7b5510f16da6dc860fd7
Anderson Pavia <andersonaap@hotmail.com>
2012-02-29 18:42:21 -0300

    chapter05

---
+++ partI/chapter05/Dollar.java
@@ -0,0 +1,20 @@
+
+class Dollar {
+
+	private int amount;
+
+	Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	Dollar times(int multiplier) {
+		return new Dollar(this.amount * multiplier);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		Dollar dollar = (Dollar)object;
+		return this.amount == dollar.amount;
+	}
+	
+}
---
+++ partI/chapter05/TestClass.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestClass {
+
+	// (-) $5 + 10 CHF = $10 se taxa e de 2:1
+	// (+) $5 * 2 = $10
+	// (+) tornar a quantidade privada
+	// (+) efeitos colaterais em Dollar
+	// (-) Arredondamento de dinheiro
+	// (+) equals
+	// (-) hashCode
+	// (-) Igualdade de null
+	// (-) Igualdade de objeto
+	// (+) 5 CHF * 2 = 10 CHF
+	// (-) Duplicacao de Dolar/Franco
+	// (-) Igualdade comum
+	// (-) Multiplicacao comum
+	
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+	
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+}
