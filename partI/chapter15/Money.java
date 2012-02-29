893cdb0d97951874983307b080670e3f1ded6d8a
Anderson Pavia <andersonaap@hotmail.com>
2012-02-29 18:43:55 -0300

    chapter15

---
+++ partI/chapter15/Money.java
@@ -0,0 +1,49 @@
+
+ class Money implements Expression {
+	protected int amount;
+	protected String currency;
+	
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		Money money = (Money)object;
+		return amount == money.amount 
+			&& currency().equals(money.currency());
+	}
+	
+	@Override
+	public String toString() {
+		return amount + " " + currency;
+	}
+
+	@Override
+	public Money reduce(Bank bank, String to) {
+		int rate = bank.rate(currency, to);
+		return new Money(amount / rate, to);
+	}
+
+	@Override
+	public Expression plus(Expression addend) {
+		return new Sum(this, addend);
+	}
+	
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
+
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
+	
+	Expression times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	};
+
+	String currency() {
+		return currency;
+	}
+}
