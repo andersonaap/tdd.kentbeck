eb884b3a0f35e547357f8ec08d95e08706fa6f4a
Anderson Pavia <andersonaap@hotmail.com>
2012-02-29 18:43:41 -0300

    chapter13

---
+++ partI/chapter13/Bank.java
@@ -0,0 +1,8 @@
+
+class Bank {
+
+	Money reduce(Expression source, String to) {
+		return source.reduce(to);
+	}
+
+}
---
+++ partI/chapter13/Money.java
@@ -0,0 +1,48 @@
+
+ class Money implements Expression {
+	protected int amount;
+	protected String currency;
+	
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		Money money = (Money)object;
+		return amount == money.amount 
+			&& currency().equals(money.currency());
+	}
+	
+	@Override
+	public String toString() {
+		return amount + " " + currency;
+	}
+
+	@Override
+	public Money reduce(String to) {
+		return this;
+	}
+
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
+
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
+	
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	};
+
+	String currency() {
+		return currency;
+	}
+
+	Expression plus(Money addend) {
+		return new Sum(this, addend);
+	}
+
+}
