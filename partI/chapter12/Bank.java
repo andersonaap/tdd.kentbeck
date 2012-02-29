893215ab689983e20d6327e0a58167248922be1e
Anderson Pavia <andersonaap@hotmail.com>
2012-02-29 18:43:36 -0300

    chapter12

---
+++ partI/chapter12/Bank.java
@@ -0,0 +1,8 @@
+
+class Bank {
+
+	Money reduce(Expression source, String to) {
+		return Money.dollar(10);
+	}
+
+}
---
+++ partI/chapter12/TestClass.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class TestClass {
+
+	// (-) $5 + 10 CHF = $10 se taxa e de 2:1
+	// (-) $5 + $5 = $10
+
+	
+	@Test
+	public void testMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10), five.times(2));
+		assertEquals(Money.dollar(15), five.times(3));
+	}
+
+	@Test
+	public void testEquality() {
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
+	}
+	
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+	
+	@Test
+	public void testSimpleAddition() {
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduce = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(10), reduce);
+	}
+}
