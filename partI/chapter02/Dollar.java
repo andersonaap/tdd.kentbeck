ca02a9a978e1f1fad36f8fea14d94ab6ac72c2c0
Anderson Pavia <andersonaap@hotmail.com>
2012-02-29 18:41:52 -0300

    chapter02

---
+++ partI/chapter02/Dollar.java
@@ -0,0 +1,14 @@
+
+class Dollar {
+
+	int amount;
+
+	Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	Dollar times(int multiplier) {
+		return new Dollar(this.amount * multiplier);
+	}
+
+}
---
+++ partI/chapter02/TestClass.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestClass {
+
+	// (-) $5 + 10 CHF = $10 se taxa e de 2:1
+	// (+) $5 * 2 = $10
+	// (-) tornar a quantidade privada
+	// (-) efeitos colaterais em Dollar
+	// (-) Arredondamento de dinheiro
+	
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
+	}
+
+}
